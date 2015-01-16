d80d9e88360afb4f3589d645b345721843bed95e
sajin <sajinrules@gmail.com>
2015-01-16 18:32:21 +0530

    test commit after ssh key add

--- scripts/controllers/firstCtrl.js
+++ scripts/controllers/firstCtrl.js
@@ -1,5 +1,7 @@
 angular.module('testApp',['btford.socket-io'])
 	.controller('firstCtrl',['$scope','ShareData','socketFactory',function($scope,ShareData,socketFactory){
+		//service has two binding
 		$scope.data=ShareData;
+
 		console.log("socketFactory:",socketFactory);
 	}]);
